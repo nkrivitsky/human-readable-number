0f9b184290ac2a4fcd0e1a9fea8c34d1d3d35437
Nikita krivitsky <nikdemoth1@gmail.com>
2020-10-01 23:10:39 +0300

    feat: implement main logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let numberStrings = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+    let dozens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    if (number == 0 || number == undefined) return "zero"
+
+    let resulltString = ""
+
+    resulltString += Math.trunc(number / 100) > 0 ? numberStrings[Math.trunc(number / 100)] + " hundred" : ""
+    number = number % 100
+    if (number < 20) {
+        resulltString += " " + numberStrings[number]
+        number = 0
+    } else {
+        resulltString += " " + dozens[Math.trunc(number / 10) - 2]
+        number = number % 10
+    }
+
+    if (number > 0) {
+        resulltString += " " + numberStrings[number]
+    }
+
+    return resulltString.trim()
 }
